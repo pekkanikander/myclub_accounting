c76d5f58ff59edc735097d4159fd675d98939ecc
Pekka Nikander <pekka.nikander@iki.fi>
2017-08-13 16:55:17 +0300

    More work on check.js

--- src/check.js
+++ src/check.js
@@ -4,9 +4,20 @@
 
 import assert       from 'assert';
 import config       from 'config';
-import * as fetch   from './fetch.js';
-import Transactions from './transactions.js';
 import {DataStream} from 'scramjet';
+
+import * as fetch   from './fetch';
+import Transactions from './transactions';
+import logger       from './log';
+
+/**
+ * Generates a payment report for the given member
+ */
+function member_to_payment_report(member) {
+    if (member.member) member = member.member;
+
+    const result = undefined;
+}
 
 /**
  * Checks the total balance of a given member's invoces
@@ -14,7 +25,7 @@ import {DataStream} from 'scramjet';
  * @param transactions A Scramjet Stream of bank transactions
  * @returns 
  */
-async function check_member_payments(member, transactions) {
+function associate_payments_transactions(member, transactions) {
     // Max four days of difference
     const DATE_EPSILON = Math.abs(new Date(1970, 0, 5, 0).valueOf());
 
@@ -23,19 +34,19 @@ async function check_member_payments(member, transactions) {
      */
     function tp_match(reference, transaction, payment) {
 	if (transaction.reference == payment.reference) {
-	    console.log("Matched with ref: " + reference);
+	    logger.info("Matched with ref: " + reference);
 	    return 1;
 	}
 
 	if (transaction.amount != payment.amount) {
-	    console.log("Different amounts: " +
+	    logger.info("Different amounts: " +
 			transaction.amount + " ≠ " + payment.amount);
 	    return 0;
 	}
 	const tdate = transaction.date.valueOf();
 	const pdate = payment.payment_date.valueOf();
 	if (Math.abs(tdate - pdate) <= DATE_EPSILON) {
-	    console.log("Matched with date: " + reference);
+	    logger.info("Matched with date: " + reference);
 	    return 1;
 	}
 	return 0;
@@ -48,7 +59,7 @@ async function check_member_payments(member, transactions) {
 	const reference = invoice.invoice.reference;
 	const transactions = T.findByReference(reference);
 	if (transactions.length == 0) {
-	    console.log("No bank transactions found for " + reference);
+	    logger.info("No bank transactions found for " + reference);
 	    return 0;
 	}
 
@@ -56,7 +67,7 @@ async function check_member_payments(member, transactions) {
 	const payments = invoice.invoice.payments
 	      .filter((payment) => (payment.amount > 0 && payment.amount < 10000));
 	if (payments.length == 0) {
-	    console.log("No payments found for " + reference);
+	    logger.info("No payments found for " + reference);
 	    return 0;
 	}
 
@@ -74,9 +85,9 @@ async function check_member_payments(member, transactions) {
 		payment.transaction = transaction;
 		return 1;
 	    }
-	    console.log("Could not match transaction and payment: ");
-	    console.log(transaction);
-	    console.log(payment);
+	    logger.info("Could not match transaction and payment: ");
+	    logger.debug(transaction);
+	    logger.debug(payment);
 	    return 0;
 	}
 
@@ -92,22 +103,74 @@ async function check_member_payments(member, transactions) {
 			  (transaction) => tp_match(reference, transaction, payment)
 		      );
 		if (matching_transactions.length == 0) {
-		    console.log("Could not find any matching transactions");
-		    console.log(transactions);
-		    console.log(payment);
+		    logger.info("Could not find any matching transactions");
+		    logger.debug(transactions);
+		    logger.debug(payment);
 		} else if (matching_transactions.length == 1) {
-		    console.log("Matched with exclusion");
+		    logger.info("Matched with exclusion");
 		    payment.transaction = matching_transactions[0];
 		} else {
-		    console.log("Found multiple matching transactions");
-		    console.log(matching_transactions);
-		    console.log(payment);
+		    logger.info("Found multiple matching transactions");
+		    logger.debug(matching_transactions);
+		    logger.debug(payment);
+		    payment.transactions = matching_transactions;
 		}
 	    }
 	);
 
 	return 0;
     }
+
+    /**
+     * Differentiate simply matching invoices from complex ones
+     *
+     * A simple invoice must have:
+     * 1. must be exactly one payment
+     * 2a) payment must be annotated with exactly one transaction
+     *   - amounts must match
+     * or 
+     * 2b) payment must be written by the bank matching
+     */
+    function isSimplyPaid(invoice) {
+	if (invoice.invoice.payments.length != 1) return false;
+
+	const payment     = invoice.invoice.payments[0];
+	const transaction = payment.transaction;
+
+	// If the payment has a matching reference, it is automatically matched
+	if (payment.reference == invoice.invoice.reference) return true;
+
+	// Otherwise the payment must be matched with a transaction
+	if (!transaction) return false;
+
+	// The amount of the transaction and the payment must be euql
+	if (transaction.amount != payment.amount) return false;
+
+	// Good enough, accept this a simply paid
+	return true;
+    }
+    
+    /**
+     * Check that all payments have exactly one transaction
+     * @returns falsy on all ok, non-zero on something needing investigation
+     */
+    function verifyAllPayments(invoice) {
+	// If there are no good payments
+	if (!invoice.payments || invoice.payments.length == 0) {
+	    return -1;
+	}
+
+	// Filter out all "good" payments, leave "bad" ones
+	const bad_payments = invoice.payments.filter(
+	    (payment) => {
+		XXX;
+	    }
+	);
+
+	return XXX;
+    }
+
+    logger.debug("Checking member " + member.member.id);
 
     assert(member.member.invoices);
 
@@ -118,25 +181,18 @@ async function check_member_payments(member, transactions) {
         // the actual invoices,  adding the due amount and status
 	.map(
 	    (i) => fetch.invoice(i.id)
-		.then(
-		    (inv) => (
-			inv.due_amount = i.due_amount,
-			inv.status     = i.status,
-			inv
-		    )
-		)
+		.reduce((out, inv) => (
+		    inv.due_amount = i.due_amount,
+		    inv.status     = i.status,
+		    inv
+		), undefined)
 	)
 
         // Augment the invoice payments with transactions
 	.each((inv) => transactions2payments(transactions, inv))
-
-        // XXX Continue here
-
-        //.map(console.log)
     ;
 
-
-    return true;
+    return member;
 }
 
 /**
@@ -145,11 +201,11 @@ async function check_member_payments(member, transactions) {
  * @param transactions A Scramjet Stream of bank transactions
  * @return A Scramejet Stream of members who have paid too much or too little
  */
-async function check_members_payments(members, transactions) {
-    (await members).filter(
-	// Check invoices balance for this member, return true/false
-	member => check_member_payments(member, transactions)
-    );
+function check_members_payments(members, transactions) {
+    members
+	.map(member => associate_payments_transactions(member, transactions))
+	// .stringify(member_to_payment_report)
+    ;
 }
 
 /**
@@ -158,7 +214,7 @@ async function check_members_payments(members, transactions) {
 if (typeof require !== 'undefined' && require.main === module) {
 
     process.on('unhandledRejection', function(reason, p) {
-	console.log('Unhandled Rejection at:', p, 'reason:', reason);
+	logger.error('Unhandled Rejection at:', p, 'reason:', reason);
 	throw reason;
     });
 
@@ -169,14 +225,10 @@ if (typeof require !== 'undefined' && require.main === module) {
 	case 'payments':
 	    const members      = fetch.members(config.group);
 	    const transactions = new Transactions()
-	    transactions.from(argv.f).then(() =>
-		{
-		    check_members_payments(members, transactions).then(
-			problems => problems
-//			    .stringify(JSON.stringify)
-//			    .pipe(process.stdout)
-		    );
-		});
+	    transactions.from(argv.f).then(() => {
+		check_members_payments(members, transactions)
+		;
+	    });
 	    break;
 	}
     });
