ec162b494dd3a16c42c3cf639525c3f9bc109439
Pekka Nikander <pekka.nikander@iki.fi>
2017-08-14 14:50:33 +0300

    Add invoices DB

---
+++ src/invoices.js
@@ -0,0 +1,29 @@
+/**
+ * Provide a db collection of invoices
+ */
+
+import {DataStream, StringStream} from 'scramjet';
+
+import config from 'config';
+import assert from 'assert';
+
+import Collection from './collection';
+import logger     from './log';
+
+/**
+ * An Invoices database
+ */
+export default class Invoices extends Collection {
+    /**
+     * Creates a new empty Invoices db collection
+     *
+     * Note that it is not a good idea to fill the db here,
+     * as filling the db is an async operatoin and the
+     * constructor should not (cannot?) return a Promise.
+     */
+    constructor(options) {
+	super('invoices', options);
+    }
+
+    
+}
